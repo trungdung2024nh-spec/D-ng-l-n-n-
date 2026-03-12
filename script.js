b51d44015de7937089039489fd8a662714297ead
trungdung2024nh-spec <trungdung2024nh@gmail.com>
2026-03-12 21:14:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
+// hiển thị bình luận khi mở trang
+window.onload = function(){
+
+let comments = JSON.parse(localStorage.getItem("comments")) || []
+
+let box = document.getElementById("comments")
+
+comments.forEach(function(c){
+
+let div = document.createElement("div")
+div.className="comment"
+
+div.innerHTML = "<b>"+c.name+"</b>: "+c.text
+
+box.appendChild(div)
+
+})
+
+}
+
+
+
 function addComment(){
 
-let name=document.getElementById("name").value
-let text=document.getElementById("text").value
+let name = document.getElementById("name").value
+let text = document.getElementById("text").value
 
 if(name=="" || text=="") return
+
+
+let comments = JSON.parse(localStorage.getItem("comments")) || []
+
+comments.push({
+name:name,
+text:text
+})
+
+
+localStorage.setItem("comments",JSON.stringify(comments))
+
 
 let box=document.getElementById("comments")
 
@@ -14,6 +48,8 @@ div.innerHTML="<b>"+name+"</b>: "+text
 
 box.appendChild(div)
 
+
 document.getElementById("name").value=""
 document.getElementById("text").value=""
+
 }
