f76a12d0af0eeb6a4d18ab14f5bd57705635e97a
trungdung2024nh-spec <trungdung2024nh@gmail.com>
2026-03-13 12:29:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-// hiển thị bình luận khi mở trang
-window.onload = function(){
+window.onload=function(){
+showComments()
+}
 
-let comments = JSON.parse(localStorage.getItem("comments")) || []
+function addComment(){
 
-let box = document.getElementById("comments")
+let name=document.getElementById("name").value
+let text=document.getElementById("text").value
 
-comments.forEach(function(c){
+if(name==""||text=="") return
 
-let div = document.createElement("div")
+let comments=JSON.parse(localStorage.getItem("comments"))||[]
+
+let time=new Date().toLocaleString()
+
+comments.push({name,text,time})
+
+localStorage.setItem("comments",JSON.stringify(comments))
+
+document.getElementById("name").value=""
+document.getElementById("text").value=""
+
+showComments()
+}
+
+function showComments(){
+
+let comments=JSON.parse(localStorage.getItem("comments"))||[]
+
+let box=document.getElementById("comments")
+
+box.innerHTML=""
+
+comments.forEach((c,i)=>{
+
+let div=document.createElement("div")
+
 div.className="comment"
 
-div.innerHTML = "<b>"+c.name+"</b>: "+c.text
+div.innerHTML=
+`<span class="delete" onclick="deleteComment(${i})">X</span>
+<b>${c.name}</b> <span class="time">${c.time}</span>
+<p>${c.text}</p>`
 
 box.appendChild(div)
 
@@ -18,38 +48,14 @@ box.appendChild(div)
 
 }
 
+function deleteComment(i){
 
+let comments=JSON.parse(localStorage.getItem("comments"))||[]
 
-function addComment(){
-
-let name = document.getElementById("name").value
-let text = document.getElementById("text").value
-
-if(name=="" || text=="") return
-
-
-let comments = JSON.parse(localStorage.getItem("comments")) || []
-
-comments.push({
-name:name,
-text:text
-})
-
+comments.splice(i,1)
 
 localStorage.setItem("comments",JSON.stringify(comments))
 
-
-let box=document.getElementById("comments")
-
-let div=document.createElement("div")
-div.className="comment"
-
-div.innerHTML="<b>"+name+"</b>: "+text
-
-box.appendChild(div)
-
-
-document.getElementById("name").value=""
-document.getElementById("text").value=""
+showComments()
 
 }
